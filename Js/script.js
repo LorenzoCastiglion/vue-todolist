20f399f40a4e463c453038c435fa951f8fd20cb1
Lorenzo Castiglion <lorenzo.castiglion@hotmail.it>
2022-10-28 17:15:29 +0200

    esercizoi terminato, stile in costruzione

--- Js/script.js
+++ Js/script.js
@@ -1,10 +1,23 @@
-
+// Rifare l'esercizio della to do list argomento dell to-do list a piacere :partying_face:.
+// Questa volta però ogni todo sarà un oggetto, formato da due proprietà:
+// - text, una stringa che indica il testo del todo
+// - done, un booleano (true/false) che indica se il todo è stato fatto oppure no
+// MILESTONE 1
+// Stampare all'interno di una lista, un item per ogni todo.
+// Se la proprietà done è uguale a true, visualizzare il testo del todo sbarrato.
+// MILESTONE 2
+// Visualizzare a fianco ad ogni item ha una "x": cliccando su di essa, il todo viene rimosso dalla lista.
+// MILESTONE 3
+// Predisporre un campo di input testuale e un pulsante "aggiungi": cliccando sul pulsante, il testo digitato viene letto e utilizzato per creare un nuovo todo, che quindi viene aggiunto alla lista dei todo esistenti.
+// Bonus:
+// 1- oltre al click sul pulsante, intercettare anche il tasto ENTER per aggiungere il todo alla lista
+// 2- cliccando sul testo dell'item, invertire il valore della proprietà done del todo corrispondente (se done era uguale a false, impostare true e viceversa)
 
 
 
 const {createApp} = Vue;
-
-let id = 0
+let done;
+let id = 0;
 const app = createApp({
     data() {
       return {
@@ -23,7 +36,7 @@ const app = createApp({
         this.newTodo = ''
       },
       removeTodo(todo) {
-        this.todos = this.todos.filter((t) => t !== todo)
+        this.todos = this.todos.filter((btn) => btn !== todo)
       },
 
       toggle(index){
